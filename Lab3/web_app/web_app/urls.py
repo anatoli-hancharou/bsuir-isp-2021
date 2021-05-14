0c671fdbcb7460a5cab46f57d6f2730bb643f83c
Anatoli Hancharou <sonyericsson2018@mail.ru>
2021-05-14 23:11:34 +0300

    Add third lab skeleton. Add simple home and log in pages. Add log in form.

---
+++ Lab3/web_app/web_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('my_app.urls'), name='home'),
+    path('account/', include('account.urls')),
+    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

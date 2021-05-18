ffa743463bfead56be0e299c07ac1d0400a0a4d3
Anatoli Hancharou <sonyericsson2018@mail.ru>
2021-05-18 23:43:35 +0300

    Add delete, update, edit post operations. Add tests for them.Change appearence using Bootstrap framework.

--- Lab3/web_app/blog/models.py
+++ Lab3/web_app/blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    categories = models.ManyToManyField(Category, related_name='topics')
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
@@ -11,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
--- Lab3/web_app/blog/views.py
+++ Lab3/web_app/blog/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.contrib import messages
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -10,3 +13,50 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        obj.save()
+        return super(BlogCreateView, self).form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+    def get_object(self, queryset=None):
+        """
+        Check the logged in user is the owner of the object or 404
+        """
+        obj = super(BlogUpdateView, self).get_object(queryset)
+        if obj.author != self.request.user:
+            messages.error(self.request, "You can't edit this post")
+            raise Http404("You don't own this object")
+        return obj
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        """
+        Check the logged in user is the owner of the object or 404
+        """
+        obj = super(BlogDeleteView, self).get_object(queryset)
+        if obj.author != self.request.user:
+            messages.error(self.request, "You can't edit this post")
+            raise Http404("You don't own this object")
+        return obj
+
+
